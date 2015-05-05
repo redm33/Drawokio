ac15c45399500c309f836b001ab39972cc3b57e7
3KS <cookiemonster2005@gmail.com>
2015-05-04 21:12:42 -0500

    new beginning and controls

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -6,7 +6,12 @@ public class MenuController : MonoBehaviour {
 
     public GameObject mainMenu;
     public GameObject pauseMenu;
-
+	public GameObject controlsMenu;
+	public GameObject pauseResume;
+	public GameObject pauseControls;
+	public GameObject pauseMain;
+	public GameObject pauseDesktop;
+		
     private MenuUIStates _state;
 
     public MenuUIStates state {
@@ -17,16 +22,22 @@ public class MenuController : MonoBehaviour {
                     Debug.Log("set menu none");
                     mainMenu.SetActive(false);
                     pauseMenu.SetActive(false);
+					controlsMenu.SetActive(false);
                     break;
                 case MenuUIStates.MAIN:
                     Debug.Log("set menu main");
                     mainMenu.SetActive(true);
                     pauseMenu.SetActive(false);
+					controlsMenu.SetActive(false);
                     break;
                 case MenuUIStates.PAUSE:
                     Debug.Log("drew pause menu");
                     mainMenu.SetActive(false);
                     pauseMenu.SetActive(true);
+					pauseResume.SetActive(true);
+					pauseControls.SetActive(true);
+					pauseMain.SetActive(true);
+					pauseDesktop.SetActive(true);
                     break;
             }
             _state = value;
@@ -50,6 +61,23 @@ public class MenuController : MonoBehaviour {
         Player.instance.paused = false;
         Room.instance.state = Room.State.PLAYING;
     }
+
+	public void OpenControls() {
+		pauseResume.SetActive(false);
+		pauseControls.SetActive(false);
+		pauseMain.SetActive(false);
+		pauseDesktop.SetActive(false);
+		controlsMenu.SetActive (true);
+	}
+
+	public void CloseControls() {
+		pauseResume.SetActive(true);
+		pauseControls.SetActive(true);
+		pauseMain.SetActive(true);
+		pauseDesktop.SetActive(true);
+		controlsMenu.SetActive (false);
+	}
+
     public void ReturnToMain() {
         Room.instance.state = Room.State.MENU_MAIN;
         Player.instance.GetComponent<PlayerDrivingController>().Jump();
